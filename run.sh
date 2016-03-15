c2da652dc1f38177ec453a614394cfedc90fd39c
Emerson Max de Medeiros Silva <emersonmax@mobister.com.br>
2016-03-15 13:37:56 -0300

    Mais atualização do script.

--- run.sh
+++ run.sh
@@ -11,15 +11,9 @@ echo "Configuring..."
 cmake ..
 
 echo "Building..."
-rm -f $EXECUTABLE
 make
 popd > /dev/null
 
 echo "Running..."
-if [[ $BUILD_DIR/$EXECUTABLE ]]
-then
-    echo "Exit with error."
-else
-    echo "Done."
-fi
-
+$BUILD_DIR/$EXECUTABLE
+echo "Done."
