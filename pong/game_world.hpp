17d6b3a3771f7c757d47d431b6ae8717de411885
Emerson MX <emersonmx@gmail.com>
2016-03-18 00:59:17 -0300

    Hello world do mundo físico.

---
+++ pong/game_world.hpp
@@ -0,0 +1,69 @@
+#ifndef PONG_GAME_WORLD_HPP
+#define PONG_GAME_WORLD_HPP
+
+#include <memory>
+
+#include <Box2D/Box2D.h>
+
+#include "pong/defs.hpp"
+
+namespace pong {
+
+class GameWorld {
+    public:
+        b2Body* box() { return box_; }
+        b2Body* ground() { return ground_; }
+
+        void create() {
+            b2Vec2 gravity(0, -10);
+            world.reset(new b2World(gravity));
+
+            ground_ = createGround();
+            box_ = createBox(); 
+        }
+
+        void update() {
+            world->Step(GAME_TIME_STEP, GAME_VELOCITY_ITERATIONS, 
+                        GAME_POSITION_ITERATIONS);
+        }
+
+    protected:
+        virtual b2Body* createBox() {
+            b2BodyDef boxDef;
+            boxDef.type = b2_dynamicBody;
+            boxDef.position.Set((WINDOW_WIDTH / 2) / (float) PIXELS_PER_METER, 
+                                (WINDOW_HEIGHT - 100) / (float) PIXELS_PER_METER);
+            b2Body* body = world->CreateBody(&boxDef);
+            b2PolygonShape dynamicBox;
+            dynamicBox.SetAsBox(25 / (float) PIXELS_PER_METER,
+                                25 / (float) PIXELS_PER_METER);
+            b2FixtureDef fixDef; 
+            fixDef.shape = &dynamicBox;
+            fixDef.density = 1;
+            fixDef.friction = 0.3;
+            body->CreateFixture(&fixDef);
+
+            return body;
+        }
+
+        virtual b2Body* createGround() {
+            b2BodyDef groundDef;
+            groundDef.position.Set((WINDOW_WIDTH / 2) / (float) PIXELS_PER_METER, 
+                                   10 / (float) PIXELS_PER_METER);
+            b2Body* body = world->CreateBody(&groundDef);
+            b2PolygonShape groundBox;
+            groundBox.SetAsBox(WINDOW_WIDTH / (float) PIXELS_PER_METER,
+                               10 / (float) PIXELS_PER_METER);
+            body->CreateFixture(&groundBox, 0);
+
+            return body;
+        }
+
+    private: 
+        std::unique_ptr<b2World> world;
+        b2Body* box_;
+        b2Body* ground_;
+};
+
+} /* namespace pong */
+#endif /* PONG_GAME_WORLD_HPP */
