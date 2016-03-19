3a00ec71d75534f1083a27b56dcc72d2c2679cab
Emerson MX <emersonmx@gmail.com>
2016-03-19 13:21:08 -0300

    Remove game objects

--- pong/game_objects.cpp
+++
@@ -1,7 +0,0 @@
-#include "pong/game_objects.hpp"
-
-namespace pong {
-
-
-
-} /* namespace pong */ 
--- pong/game_objects.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef PONG_GAME_OBJECTS_HPP
-#define PONG_GAME_OBJECTS_HPP
-
-namespace pong {
-
-
-
-} /* namespace pong */ 
-#endif /* PONG_GAME_OBJECTS_HPP */
