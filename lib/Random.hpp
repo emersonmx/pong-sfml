113ea6eb189ddaed90345f4385f84240566b9d0b
Emerson Max de Medeiros Silva <emersonmax@mobister.com.br>
2016-03-23 13:38:01 -0300

    Add Random class

---
+++ lib/Random.hpp
@@ -0,0 +1,34 @@
+#ifndef RANDOM_HPP
+#define RANDOM_HPP
+
+#include <random>
+
+#include <SFML/Graphics.hpp>
+
+class Random {
+    public:
+        int nextInt() {
+            std::mt19937 rng(device());
+            std::uniform_int_distribution<> uniform;
+            return uniform(rng);
+        }
+        int nextInt(int min, int max) {
+            std::mt19937 rng(device());
+            std::uniform_int_distribution<> uniform(min, max);
+            return uniform(rng);
+        }
+        float nextFloat() {
+            std::mt19937 rng(device());
+            std::uniform_real_distribution<> uniform;
+            return uniform(rng);
+        }
+        float nextFloat(float min, float max) {
+            std::mt19937 rng(device());
+            std::uniform_real_distribution<> uniform(min, max);
+            return uniform(rng);
+        }
+    private:
+        std::random_device device;
+};
+
+#endif /* RANDOM_HPP */
