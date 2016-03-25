710e76673bc07ccfe0e2f683bf2d2e341a15a6e0
Emerson MX <emersonmx@gmail.com>
2016-03-24 22:20:00 -0300

    Align parameters

--- pong/game_state.cpp
+++ pong/game_state.cpp
@@ -19,7 +19,7 @@ void syncBodyToTransformable(b2Body* body, sf::Transformable& transformable) {
     b2Vec2 position = body->GetPosition();
     float angle = body->GetAngle();
     transformable.setPosition(position.x * PIXELS_PER_METER,
-                          position.y * PIXELS_PER_METER);
+                              position.y * PIXELS_PER_METER);
     transformable.setRotation(angle);
 }
 
