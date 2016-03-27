acf2885954c0c52fb5aa1b4b0141a67242d9ca6c
Emerson MX <emersonmx@gmail.com>
2016-03-27 04:41:17 -0300

    Move helpers

--- pong/game_state.cpp
+++ pong/game_state.cpp
@@ -7,6 +7,22 @@
 
 namespace pong {
 
+sf::RectangleShape createRectangleShape(float width, float height) {
+    sf::RectangleShape shape;
+    shape.setOrigin(width / 2, height / 2);
+    shape.setSize(sf::Vector2f(width, height));
+    shape.setFillColor(sf::Color::White);
+    return shape;
+}
+
+void syncBodyToTransformable(b2Body* body, sf::Transformable& transformable) {
+    b2Vec2 position = body->GetPosition();
+    float angle = body->GetAngle();
+    transformable.setPosition(position.x * PIXELS_PER_METER,
+                              position.y * PIXELS_PER_METER);
+    transformable.setRotation(angle);
+}
+
 void GameState::enter(Game* game) {
     DefaultState::enter(game);
 
