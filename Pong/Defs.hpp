41d13e985bc445fb8fbdf8da8830283e8e4dd629
Emerson MX <emersonmx@gmail.com>
2016-05-03 00:22:27 -0300

    Remove unused enum

--- Pong/Defs.hpp
+++ Pong/Defs.hpp
@@ -11,12 +11,6 @@ constexpr const int WINDOW_HEIGHT = 480;
 constexpr const float WINDOW_HALF_WIDTH = WINDOW_WIDTH / 2.0f;
 constexpr const float WINDOW_HALF_HEIGHT = WINDOW_HEIGHT / 2.0f;
 
-enum class Player {
-    NONE,
-    PLAYER_1, PLAYER_2,
-    PLAYER_COUNT
-};
-
 constexpr const int PIXELS_PER_METER = 64;
 
 constexpr const int GAME_FRAMES_PER_SECOND = 60;
